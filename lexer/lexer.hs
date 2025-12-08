c7a7c96fb3e5d5ae06426497c37c17a70c5265c5
koraynilay <koray.fra@gmail.com>
2025-12-08 21:22:55 +0100

    Improve formatting, remove unused functions, rename variable from stripPrefixRegex to avoid confusion
    sad, I liked those functions I had to remove :(

--- lexer/lexer.hs
+++ lexer/lexer.hs
@@ -19,88 +19,87 @@ stripPrefixRegex re xs | mid r == "" = Nothing
 
 -- TokenName            Pattern                         Nome
 -- -------------------------------------------------------
--- Numeri           Costante numerica                256
--- Identificatore   Lettera seguita da lettere       257
---                  e cifre
--- Print            print                            258
--- While            while                            259
--- Do               do                               260
--- Conditional      conditional                      261
--- Case             case                             262
--- Break            break                            263
--- Default          default                          264
--- Input utente     user                             265
--- Assegnamento     :=                               266
--- Congiunzione     &&                               267
--- Disgiunzione     ||                               268
--- Minore/uguale    <=                               269
--- Maggiore/uguale  >=                               270
--- Uguale a         ==                               271
--- Diverso da       !=                               272
--- Minore di        <                                60
--- Maggiore di      >                                62
--- Par. tonda (     (                                40
--- Par. tonda )     )                                41
--- Par. quadra [    [                                91
--- Par. quadra ]    ]                                93
--- Par. graffa {    {                                123
--- Par. graffa }    }                                125
--- Somma            +                                43
--- Sottrazione      -                                45
--- Moltiplicaz.     *                                42
--- Divisione        /                                47
--- Punto e virgola  ;                                59
--- Virgola          ,                                44
--- EOF              Fine input                       -1
+-- Numeri           Costante numerica                   256
+-- Identificatore   Lettera seguita da lettere e cifre  257
+-- Print            print                               258
+-- While            while                               259
+-- Do               do                                  260
+-- Conditional      conditional                         261
+-- Case             case                                262
+-- Break            break                               263
+-- Default          default                             264
+-- Input utente     user                                265
+-- Assegnamento     :=                                  266
+-- Congiunzione     &&                                  267
+-- Disgiunzione     ||                                  268
+-- Minore/uguale    <=                                  269
+-- Maggiore/uguale  >=                                  270
+-- Uguale a         ==                                  271
+-- Diverso da       !=                                  272
+-- Minore di        <                                   60
+-- Maggiore di      >                                   62
+-- Par. tonda (     (                                   40
+-- Par. tonda )     )                                   41
+-- Par. quadra [    [                                   91
+-- Par. quadra ]    ]                                   93
+-- Par. graffa {    {                                   123
+-- Par. graffa }    }                                   125
+-- Somma            +                                   43
+-- Sottrazione      -                                   45
+-- Moltiplicaz.     *                                   42
+-- Divisione        /                                   47
+-- Punto e virgola  ;                                   59
+-- Virgola          ,                                   44
+-- EOF              Fine input                          -1
 
-data TokenName = Number        | Identifier
-           | Print         | While        | Do           | Conditional | Case
-           | Break         | Default      | UserInput    | Assignment  | Conjunction
-           | Disjunction   | LessEqual    | GreaterEqual | Equal       | NotEqual
-           | LessThan      | GreaterThan  | ParenOpen    | ParenClose
-           | BracketOpen   | BracketClose | BraceOpen    | BraceClose
-           | Plus          | Minus        | Multiply     | Divide
-           | Semicolon     | Comma        | EOF
-           deriving (Show, Eq)
+data TokenName = Number        | Identifier   | Print        | While
+               | Do            | Conditional  | Case         | Break
+               | Default       | UserInput    | Assignment   | Conjunction
+               | Disjunction   | LessEqual    | GreaterEqual | Equal
+               | NotEqual      | LessThan     | GreaterThan  | ParenOpen
+               | ParenClose    | BracketOpen  | BracketClose | BraceOpen
+               | BraceClose    | Plus         | Minus        | Multiply
+               | Divide        | Semicolon    | Comma        | EOF
+                deriving (Show, Eq)
 
 getTokenName :: TokenName -> Int
-getTokenName Number = 256
-getTokenName Identifier = 257
-getTokenName Print = 258
-getTokenName While = 259
-getTokenName Do = 260
-getTokenName Conditional = 261
-getTokenName Case = 262
-getTokenName Break = 263
-getTokenName Default = 264
-getTokenName UserInput = 265
-getTokenName Assignment = 266
-getTokenName Conjunction = 267
-getTokenName Disjunction = 268
-getTokenName LessEqual = 269
-getTokenName GreaterEqual = 270
-getTokenName Equal = 271
-getTokenName NotEqual = 272
-getTokenName LessThan = 60
-getTokenName GreaterThan = 62
-getTokenName ParenOpen = 40
-getTokenName ParenClose = 41
-getTokenName BracketOpen = 91
-getTokenName BracketClose = 93
-getTokenName BraceOpen = 123
-getTokenName BraceClose = 125
-getTokenName Plus = 43
-getTokenName Minus = 45
-getTokenName Multiply = 42
-getTokenName Divide = 47
-getTokenName Semicolon = 59
-getTokenName Comma = 44
-getTokenName EOF = -1
+getTokenName Number        = 256
+getTokenName Identifier    = 257
+getTokenName Print         = 258
+getTokenName While         = 259
+getTokenName Do            = 260
+getTokenName Conditional   = 261
+getTokenName Case          = 262
+getTokenName Break         = 263
+getTokenName Default       = 264
+getTokenName UserInput     = 265
+getTokenName Assignment    = 266
+getTokenName Conjunction   = 267
+getTokenName Disjunction   = 268
+getTokenName LessEqual     = 269
+getTokenName GreaterEqual  = 270
+getTokenName Equal         = 271
+getTokenName NotEqual      = 272
+getTokenName LessThan      = 60
+getTokenName GreaterThan   = 62
+getTokenName ParenOpen     = 40
+getTokenName ParenClose    = 41
+getTokenName BracketOpen   = 91
+getTokenName BracketClose  = 93
+getTokenName BraceOpen     = 123
+getTokenName BraceClose    = 125
+getTokenName Plus          = 43
+getTokenName Minus         = 45
+getTokenName Multiply      = 42
+getTokenName Divide        = 47
+getTokenName Semicolon     = 59
+getTokenName Comma         = 44
+getTokenName EOF           = -1
 
 data Token = TokenString TokenName String
            | TokenNumber TokenName Integer
            | TokenSimple TokenName
-  deriving (Show, Eq)
+             deriving (Show, Eq)
 
 tokenName :: Token -> TokenName
 tokenName (TokenString n _) = n
@@ -112,21 +111,12 @@ tokenValue (TokenString _ s) = Just $ Left s
 tokenValue (TokenNumber _ n) = Just $ Right n
 tokenValue (TokenSimple n)   = Nothing
 
-matchIdentifier :: String -> (String, Token)
-matchIdentifier (x:xs) = (xs, TokenString Identifier iden)
-  where
-        iden = parseIdentifier (x:xs)
-
-parseIdentifier :: String -> String
-parseIdentifier (x:xs) | not $ isAlphaNum x = []
-                       | otherwise = x : parseIdentifier xs
-
 matchNumber :: String -> Token
 matchNumber xs = TokenNumber Number $ fst $ fromRight (0, T.pack "err") $ decimal $ T.pack xs
 
 matchTok :: String -> (String, Token)
 matchTok [] = ([], TokenSimple EOF)
-matchTok (stripPrefixRegex "[a-zA-Z][a-zA-Z0-9]*" -> Just xs)
+matchTok (stripPrefixRegex "[a-zA-Z][a-zA-Z0-9]*" -> Just res)
                                 | matched == "print"       = (rest, TokenString Print "print")
                                 | matched == "while"       = (rest, TokenString While "while")
                                 | matched == "do"          = (rest, TokenString Do "do")
@@ -137,8 +127,8 @@ matchTok (stripPrefixRegex "[a-zA-Z][a-zA-Z0-9]*" -> Just xs)
                                 | matched == "user"        = (rest, TokenString UserInput "user")
                                 | otherwise                = (rest, TokenString Identifier matched)
   where
-        (matched, rest) = xs
-matchTok (stripPrefixRegex "[0-9]+" -> Just xs) = (snd xs, matchNumber $ fst xs)
+        (matched, rest) = res
+matchTok (stripPrefixRegex "[0-9]+" -> Just res) = (snd res, matchNumber $ fst res)
 matchTok (stripPrefix ":=" -> Just xs) = (xs, TokenSimple Assignment)
 matchTok (stripPrefix "&&" -> Just xs) = (xs, TokenSimple Conjunction)
 matchTok (stripPrefix "||" -> Just xs) = (xs, TokenSimple Disjunction)
