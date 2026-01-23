872d2b5b96866728d2f7ed5d7b5e5e3cca15c025
koraynilay <koray.fra@gmail.com>
2026-01-23 23:38:15 +0100

    Split Token type into Token.hs from Lexer.hs

--- Lexer.hs
+++ Lexer.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE ViewPatterns #-}
 module Lexer where
 
+import Token
+
 import Data.Text.Read
 import Data.Text as T (pack, unpack)
 import Data.Char
@@ -20,100 +22,6 @@ stripPrefixRegex re xs | mid r == "" = Nothing
         r = RE.match regex xs :: (String, String, String)
         mid (x,y,z) = y
         last (x,y,z) = z
-
--- TokenName            Pattern                         Nome
--- -------------------------------------------------------
--- Numeri           Costante numerica                   256
--- Identificatore   Lettera seguita da lettere e cifre  257
--- Print            print                               258
--- While            while                               259
--- Do               do                                  260
--- Conditional      conditional                         261
--- Case             case                                262
--- Break            break                               263
--- Default          default                             264
--- Input utente     user                                265
--- Assegnamento     :=                                  266
--- Congiunzione     &&                                  267
--- Disgiunzione     ||                                  268
--- Minore/uguale    <=                                  269
--- Maggiore/uguale  >=                                  270
--- Uguale a         ==                                  271
--- Diverso da       !=                                  272
--- Minore di        <                                   60
--- Maggiore di      >                                   62
--- Par. tonda (     (                                   40
--- Par. tonda )     )                                   41
--- Par. quadra [    [                                   91
--- Par. quadra ]    ]                                   93
--- Par. graffa {    {                                   123
--- Par. graffa }    }                                   125
--- Somma            +                                   43
--- Sottrazione      -                                   45
--- Moltiplicaz.     *                                   42
--- Divisione        /                                   47
--- Punto e virgola  ;                                   59
--- Virgola          ,                                   44
--- EOF              Fine input                          -1
-
-data TokenName = Number        | Identifier   | Print        | While
-               | Do            | Conditional  | Case         | Break
-               | Default       | UserInput    | Assignment   | Conjunction
-               | Disjunction   | LessEqual    | GreaterEqual | Equal
-               | NotEqual      | LessThan     | GreaterThan  | ParenOpen
-               | ParenClose    | BracketOpen  | BracketClose | BraceOpen
-               | BraceClose    | Plus         | Minus        | Multiply
-               | Divide        | Semicolon    | Comma        | EOF
-                deriving (Show, Eq)
-
-getTokenName :: TokenName -> Int
-getTokenName Number        = 256
-getTokenName Identifier    = 257
-getTokenName Print         = 258
-getTokenName While         = 259
-getTokenName Do            = 260
-getTokenName Conditional   = 261
-getTokenName Case          = 262
-getTokenName Break         = 263
-getTokenName Default       = 264
-getTokenName UserInput     = 265
-getTokenName Assignment    = 266
-getTokenName Conjunction   = 267
-getTokenName Disjunction   = 268
-getTokenName LessEqual     = 269
-getTokenName GreaterEqual  = 270
-getTokenName Equal         = 271
-getTokenName NotEqual      = 272
-getTokenName LessThan      = 60
-getTokenName GreaterThan   = 62
-getTokenName ParenOpen     = 40
-getTokenName ParenClose    = 41
-getTokenName BracketOpen   = 91
-getTokenName BracketClose  = 93
-getTokenName BraceOpen     = 123
-getTokenName BraceClose    = 125
-getTokenName Plus          = 43
-getTokenName Minus         = 45
-getTokenName Multiply      = 42
-getTokenName Divide        = 47
-getTokenName Semicolon     = 59
-getTokenName Comma         = 44
-getTokenName EOF           = -1
-
-data Token = TokenString TokenName String
-           | TokenNumber TokenName Integer
-           | TokenSimple TokenName
-             deriving (Show, Eq)
-
-tokenName :: Token -> TokenName
-tokenName (TokenString n _) = n
-tokenName (TokenNumber n _) = n
-tokenName (TokenSimple n)   = n
-
-tokenValue :: Token -> Maybe (Either String Integer)
-tokenValue (TokenString _ s) = Just $ Left s
-tokenValue (TokenNumber _ n) = Just $ Right n
-tokenValue (TokenSimple n)   = Nothing
 
 matchNumber :: String -> Either String Token
 matchNumber xs = case decimal $ T.pack xs of
