4eae449175007624359d9b6a6e90bbbd9b4d9a62
koraynilay <koray.fra@gmail.com>
2026-01-24 22:48:44 +0100

    Add ParserOnly.hs that only checks grammar and Main_parser/lexer.hs

---
+++ Main_parser.hs
@@ -0,0 +1,27 @@
+import Token
+import ParserOnly
+import Lexer
+
+import System.Environment (getArgs, getProgName)
+
+usage :: String -> IO String
+usage s = return ("Usage: " ++ s ++ " FILE")
+
+main :: IO ()
+main = do
+        args <- getArgs
+        case args of
+                [] -> getProgName >>= usage >>= putStrLn
+                as -> handleFiles as
+
+handleFiles :: [String] -> IO ()
+handleFiles [] = return ()
+handleFiles (f:fs) = handleFile f >> handleFiles fs
+
+handleFile :: String -> IO ()
+handleFile fn = readFile fn >>= matchIO fn >>= \x -> putStrLn (fn ++ ": " ++ show (parse x))
+--handleFile fn = do f <- readFile fn
+--                   ts <- matchIO fn f
+--                   print ts
+--                   let r = parse ts
+--                   putStrLn $ fn ++ ": " ++ show r
---
+++ ParserOnly.hs
@@ -0,0 +1,160 @@
+module ParserOnly where
+
+import Token
+import Lexer
+import Instruction
+
+import qualified Data.Map as M
+import Control.Monad.State
+import Control.Applicative
+
+type CompilerStateT = StateT CompilerState Maybe
+
+data CompilerState = CompilerState
+  { labelCounter :: Int
+  , tokens       :: [Token]
+  , instructions :: [Either Instruction Label]
+  , symbolTable  :: M.Map String Int
+  , nextVarAddr :: Int
+  } deriving (Show)
+
+initialState :: CompilerState
+initialState = CompilerState
+  { labelCounter = 0
+  , tokens       = []
+  , instructions = []
+  , symbolTable  = M.empty
+  , nextVarAddr = 0
+  }
+
+parse :: [Token] -> Bool
+parse ts = case evalStateT prog $ initialState { tokens = ts } of
+                Just x -> True
+                Nothing -> False
+
+parseDebug :: [Token] -> Maybe ((), CompilerState)
+parseDebug ts = runStateT prog $ initialState { tokens = ts }
+
+nextVarAddrM :: CompilerStateT Int
+nextVarAddrM = do
+  s <- get
+  let addr = nextVarAddr s
+  put s { nextVarAddr = addr + 1 }
+  return addr
+
+-- TODO: handle variable not declared
+getVarAddr :: String -> CompilerStateT Int
+getVarAddr varName = do
+  s <- get
+  let t = symbolTable s
+  case M.lookup varName t of
+    Just addr -> return addr
+    Nothing -> do
+      newAddr <- nextVarAddrM
+      put s { symbolTable = M.insert varName newAddr t }
+      return newAddr
+
+newLabel :: CompilerStateT Label
+newLabel = do
+  s <- get
+  let labelNumber = labelCounter s
+  put $ s { labelCounter = labelNumber + 1 }
+  return $ Label $ "L" ++ show labelNumber ++ ":"
+
+getTok :: CompilerStateT Token
+getTok = do
+  s <- get
+  case tokens s of
+    (t:ts) -> do
+      put (s { tokens = ts })
+      return t
+    [] -> empty
+
+sat :: (Token -> Bool) -> CompilerStateT ()
+sat p = do
+  t <- getTok
+  if p t then return () else empty
+
+tok :: Token -> CompilerStateT ()
+tok t = sat (== t)
+
+tokId :: CompilerStateT ()
+tokId = do
+  t <- getTok
+  case t of
+    Identifier var -> return ()
+    _ -> empty
+
+tokNum :: CompilerStateT ()
+tokNum = do
+  t <- getTok
+  case t of
+    Number n -> return ()
+    _ -> empty
+
+-- prods
+
+prog :: CompilerStateT ()
+prog = (statlist >> tok EOF)
+
+statlist :: CompilerStateT ()
+statlist = (stat >> statlistp)
+
+statlistp :: CompilerStateT ()
+statlistp = (tok Semicolon >> stat >> statlistp)
+        <|> return ()
+
+stat :: CompilerStateT ()
+stat = (tokId >> tok Assignment >> assignv)
+   <|> (tok Print >> tok BracketOpen >> exprlist >> tok BracketClose)
+   <|> (tok While >> tok ParenOpen >> bexpr >> tok ParenClose >> tok Do >> stat)
+   <|> (tok Conditional >> tok BracketOpen >> caselist >> tok BracketClose >> tok Default >> stat)
+   <|> (tok CurlyOpen >> statlist >> tok CurlyClose)
+
+assignv :: CompilerStateT ()
+assignv = (tok UserInput) <|> expr
+
+caselist :: CompilerStateT ()
+caselist = (caseitem >> caselistp)
+
+caselistp :: CompilerStateT ()
+caselistp = (caseitem >> caselistp)
+        <|> return ()
+
+caseitem :: CompilerStateT ()
+caseitem = (tok Case >> tok ParenOpen >> bexpr >> tok ParenClose >> tok Do >> stat >> caseitemd)
+
+caseitemd :: CompilerStateT ()
+caseitemd = (tok Break) <|> return ()
+
+bexpr :: CompilerStateT ()
+bexpr = (relop >> expr >> expr)
+    <|> (tok Conjunction >> bexpr >> bexpr)
+    <|> (tok Disjunction >> bexpr >> bexpr)
+
+expr :: CompilerStateT ()
+expr = (tok Plus >> operands)
+   <|> (tok Minus >> expr >> expr)
+   <|> (tok Multiply >> operands)
+   <|> (tok Divide >> expr >> expr)
+   <|> (tokNum)
+   <|> (tokId)
+
+operands :: CompilerStateT ()
+operands = (expr >> expr)
+       <|> (tok BracketOpen >> exprlist >> tok BracketClose)
+
+exprlist :: CompilerStateT ()
+exprlist = (expr >> exprlistp)
+
+exprlistp :: CompilerStateT ()
+exprlistp = (tok Comma >> expr >> exprlistp)
+        <|> return ()
+
+relop :: CompilerStateT ()
+relop = (tok GreaterEqual)
+    <|> (tok LessEqual)
+    <|> (tok Equal)
+    <|> (tok GreaterThan)
+    <|> (tok LessThan)
+    <|> (tok NotEqual)
