75022c6335d3c4b3dff2ca8e2e528c38a87ba6a5
koraynilay <koray.fra@gmail.com>
2026-01-24 02:27:38 +0100

    Start implementing Parser.hs using State Monad and Alternative (<|>)

--- Parser.hs
+++ Parser.hs
@@ -1,30 +1,173 @@
+module Parser where
+
+import Token
 import Lexer
+import Instruction
 
-data Prog = StatList EOF
-data StatList = Stat StatListP
-data StatListP = Semicolon Stat StatListP | Empty
-data Stat = Print BracketOpen ExprList BracketClose
-          | Identifier Assignment Expr
-          | Identifier Assignment UserInput
-          | While ParenOpen BExpr ParenClose Do Stat
-          | Conditional BracketOpen CaseList BracketClose Default Stat
-          | BraceOpen StatList BraceClose
-data CaseList = CaseItem CaseListP
-data CaseListP = CaseItem CaseListP | Empty
-data CaseItem = Case ParenOpen BExpr ParenClose Do Stat Break
-              | Case ParenOpen BExpr ParenClose Do Stat
-data BExpr = Relop Expr Expr
-data Expr = Plus Expr Expr | Minus Expr Expr
-          | Multiply Expr Expr | Divide Expr Expr
-          | Plus BracketOpen ExprList BracketClose
-          | Multiply BracketOpen ExprList BracketClose
-          | Number | Identifier
-data ExprList = Expr ExprListP
-data ExprListP = Comma Expr ExprListP | Empty
-data Relop = GreaterEqual | LessEqual | Equal | GreaterThan | LessThan | NotEqual
+import qualified Data.Map as M
+import Control.Monad.State
+import Control.Applicative
 
---prog (Prog:xs) = prog xs
---prog (x:xs) = x xs
+type CompilerStateT = StateT CompilerState Maybe
+
+data CompilerState = CompilerState
+  { labelCounter :: Int
+  , tokens       :: [Token]
+  , instructions :: [Instruction]
+  , symbolTable  :: M.Map String Int
+  , nextVarAddr :: Int
+  } deriving (Show)
+
+initialState :: CompilerState
+initialState = CompilerState
+  { labelCounter = 0
+  , tokens       = []
+  , instructions = []
+  , symbolTable  = M.empty
+  , nextVarAddr = 0
+  }
+
+nextVarAddrM :: CompilerStateT Int
+nextVarAddrM = do
+  s <- get
+  let addr = nextVarAddr s
+  put s { nextVarAddr = addr + 1 }
+  return addr
+
+-- TODO: handle variable not declared
+getVarAddr :: String -> CompilerStateT Int
+getVarAddr varName = do
+  s <- get
+  let t = symbolTable s
+  case M.lookup varName t of
+    Just addr -> return addr
+    Nothing -> do
+      newAddr <- nextVarAddrM
+      put s { symbolTable = M.insert varName newAddr t }
+      return newAddr
+
+newLabel :: CompilerStateT String
+newLabel = do
+  s <- get
+  let labelNumber = labelCounter s
+  put $ s { labelCounter = labelNumber + 1 }
+  return $ "L" ++ show labelNumber ++ ":"
+
+getTok :: CompilerStateT Token
+getTok = do
+  s <- get
+  let (t:ts) = tokens s
+  put (s { tokens = ts })
+  return t
+
+sat :: (Token -> Bool) -> CompilerStateT Token
+sat p = do
+  t <- getTok
+  if p t then return t else empty
+
+tok :: CompilerStateT Token
+tok = sat (== tok)
+
+-- prods
+
+prog :: CompilerStateT ()
+prog = statlist >> tok EOF
+
+statlist :: CompilerStateT ()
+statlist = stat >> statlistp
+       <|> tok Semicolon >> stat >> statlistp
+
+statlistp :: CompilerStateT ()
+statlistp = tok Semicolon >> stat >> statlistp
+        <|> return
+
+stat :: CompilerStateT ()
+stat = do (Identifier var) <- tok Identifier
+          tok Assignment
+          assignv
+          varAddr <- getVarAddr var
+          emit (Istore varAddr)
+   <|> tok Print >> tok BracketOpen >> exprlist >> tok BracketClose
+   <|> tok While >> tok ParenOpen >> bexpr >> tok Do >> stat
+   <|> tok Conditional >> tok BracketOpen >> caselist >> tok BracketClose >> tok Default >> stat
+   <|> tok CurlyOpen >> statlist >> tok CurlyClose
+
+assignv :: CompilerStateT ()
+assignv = do tok UserInput
+             emit InvokeRead
+      <|> expr
+
+caselist :: CompilerStateT ()
+caselist = caseitem >> caselistp
+
+caselistp :: CompilerStateT ()
+caselistp = caseitem >> caselistp
+        <|> return
+
+caseitem :: CompilerStateT ()
+caseitem = tok Case >> tok ParenOpen >> bexpr >> tok ParenClose >> tok Do >> stat >> caseitemd
+
+caseitemd :: CompilerStateT ()
+caseitemd = tok Break <|> return
+
+bexpr :: CompilerStateT ()
+bexpr = relop >> expr >> expr
+
+expr :: CompilerStateT ()
+expr = tok Plus >> operands
+   <|> tok Minus >> expr >> expr
+   <|> tok Multiply >> mult
+   <|> tok Divide >> expr expr
+   <|> do (Number n) <- tok Number
+          emit (Ldc n)
+   <|> do (Identifier var) <- tok Identifier
+          varAddr <- getVarAddr var
+          emit (Iload varAddr) -- TODO: handle variable not declared
+
+operands :: CompilerStateT ()
+operands = expr >> expr
+       <|> tok BracketOpen >> exprlist >> tok BracketOpen
+
+-- plus :: CompilerStateT ()
+-- plus = expr >> expr
+--    <|> tok BracketOpen >> exprlist >> tok BracketOpen
+-- 
+-- mult :: CompilerStateT ()
+-- mult = expr >> expr
+--    <|> tok BracketOpen >> exprlist >> tok BracketOpen
+
+exprlist :: CompilerStateT ()
+exprlist = expr >> exprlistp
+
+exprlistp :: CompilerStateT ()
+exprlistp = tok Comma >> expr >> exprlistp
+        <|> return () -- () needed?
+
+relop :: CompilerStateT ()
+relop = tok GreaterEqual >> emit 
+
+
+
+
+
+--  s <- get
+--  case tokens s of
+--    (Print:ts) -> expr >> getVarAddr var >>= \addr -> emit (Istore addr)
+--    (Identifier var:Assignment:UserInput:ts) -> put (s { tokens = ts }) >> emit InvokeRead >> getVarAddr var >>= \addr -> emit (Istore addr)
+--    (Identifier var:Assignment:ts) -> expr >> getVarAddr var >>= \addr -> emit (Istore addr)
+
+  
+
+emit :: Instruction -> CompilerStateT ()
+-- TODO: use ins : instructions s and reverse at the end for efficiency
+emit ins = modify $ \s -> s { instructions = instructions s ++ [ins] }
+
+
+--prog :: [Token] -> [Instruction]
+--prog xs = endProg $ statlist xs
 --
---statList (StatList:xs) = statList xs
---statList (x:xs) = x xs
+--endProg :: [Token] -> [Instruction]
+--endProg (TokenSimple EOF:_) = []
+--
+--statlist :: [Token] -> [Instruction]
+
