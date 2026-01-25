0a1606183c66b200558b5c06ebb32e57b6e438c8
koraynilay <koray.fra@gmail.com>
2026-01-25 02:23:20 +0100

    Use Show for Instruction(s)/Label(s) and add CodeGen.hs

--- Instruction.hs
+++ Instruction.hs
@@ -1,64 +1,11 @@
 module Instruction where
 
-header :: String
-header = unlines
-  [ ".class public Output"
-  , ".super java/lang/Object"
-  , ""
-  , ".method public <init>()V"
-  , " aload_0"
-  , " invokenonvirtual java/lang/Object/<init>()V"
-  , " return"
-  , ".end method"
-  , ""
-  , ".method public static print(I)V"
-  , " .limit stack 2"
-  , " getstatic java/lang/System/out Ljava/io/PrintStream;"
-  , " iload_0"
-  , " invokestatic java/lang/Integer/toString(I)Ljava/lang/String;"
-  , " invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V"
-  , " return"
-  , ".end method"
-  , ""
-  , ".method public static read()I"
-  , " .limit stack 3"
-  , " new java/util/Scanner"
-  , " dup"
-  , " getstatic java/lang/System/in Ljava/io/InputStream;"
-  , " invokespecial java/util/Scanner/<init>(Ljava/io/InputStream;)V"
-  , " invokevirtual java/util/Scanner/next()Ljava/lang/String;"
-  , " invokestatic java/lang/Integer.parseInt(Ljava/lang/String;)I"
-  , " ireturn"
-  , ".end method"
-  , ""
-  , ".method public static run()V"
-  , " .limit stack 1024"
-  , " .limit locals 256"
-  ]
-
-footer :: String
-footer = unlines
-  [ " return"
-  , ".end method"
-  , ""
-  , ".method public static main([Ljava/lang/String;)V"
-  , " invokestatic Output/run()V"
-  , " return"
-  , ".end method"
-  ]
-
-toJasmin :: [Either Instruction Label] -> String
-toJasmin xs = header ++ (foldl (++) "" $ map toJasminSingle xs) ++ footer
-
-toJasminSingle :: Either Instruction Label -> String
-toJasminSingle (Left  i) = instructionToJasmin i
-toJasminSingle (Right l) = labelToJasmin l
-
 data Label = Label String
-  deriving (Show, Eq)
+  deriving (Eq)
 
-labelToJasmin :: Label -> String
-labelToJasmin (Label l) = l ++ ":\n"
+instance Show Label where
+  show :: Label -> String
+  show (Label l) = l ++ ":\n"
 
 data Instruction = Dup
                  | Pop
@@ -82,28 +29,29 @@ data Instruction = Dup
                  | Goto Label
                  | InvokePrint
                  | InvokeRead
-                   deriving (Show, Eq)
+                   deriving (Eq)
 
-instructionToJasmin :: Instruction -> String
-instructionToJasmin Dup                 = " dup\n"
-instructionToJasmin Pop                 = " pop\n"
-instructionToJasmin Imul                = " imul\n"
-instructionToJasmin Ineg                = " ineg\n"
-instructionToJasmin Idiv                = " idiv\n"
-instructionToJasmin Iadd                = " iadd\n"
-instructionToJasmin Isub                = " isub\n"
-instructionToJasmin Ior                 = " ior\n"
-instructionToJasmin Iand                = " iand\n"
-instructionToJasmin (Ldc val)           = " ldc " ++ show val ++ "\n"
-instructionToJasmin (Istore addr)       = " istore " ++ show addr ++ "\n"
-instructionToJasmin (Iload addr)        = " iload " ++ show addr ++ "\n"
-instructionToJasmin (IfCmpEQ (Label l)) = " if_icmpeq " ++ l ++ "\n"
-instructionToJasmin (IfCmpLE (Label l)) = " if_icmple " ++ l ++ "\n"
-instructionToJasmin (IfCmpLT (Label l)) = " if_icmplt " ++ l ++ "\n"
-instructionToJasmin (IfCmpNE (Label l)) = " if_icmpne " ++ l ++ "\n"
-instructionToJasmin (IfCmpGE (Label l)) = " if_icmpge " ++ l ++ "\n"
-instructionToJasmin (IfCmpGT (Label l)) = " if_icmpgt " ++ l ++ "\n"
-instructionToJasmin (Ifne (Label l))    = " ifne " ++ l ++ "\n"
-instructionToJasmin (Goto (Label l))    = " goto " ++ l ++ "\n"
-instructionToJasmin InvokePrint         = " invokestatic Output/print(I)V\n"
-instructionToJasmin InvokeRead          = " invokestatic Output/read()I\n"
+instance Show Instruction where
+  show :: Instruction -> String
+  show Dup                 = " dup\n"
+  show Pop                 = " pop\n"
+  show Imul                = " imul\n"
+  show Ineg                = " ineg\n"
+  show Idiv                = " idiv\n"
+  show Iadd                = " iadd\n"
+  show Isub                = " isub\n"
+  show Ior                 = " ior\n"
+  show Iand                = " iand\n"
+  show (Ldc val)           = " ldc " ++ show val ++ "\n"
+  show (Istore addr)       = " istore " ++ show addr ++ "\n"
+  show (Iload addr)        = " iload " ++ show addr ++ "\n"
+  show (IfCmpEQ (Label l)) = " if_icmpeq " ++ l ++ "\n"
+  show (IfCmpLE (Label l)) = " if_icmple " ++ l ++ "\n"
+  show (IfCmpLT (Label l)) = " if_icmplt " ++ l ++ "\n"
+  show (IfCmpNE (Label l)) = " if_icmpne " ++ l ++ "\n"
+  show (IfCmpGE (Label l)) = " if_icmpge " ++ l ++ "\n"
+  show (IfCmpGT (Label l)) = " if_icmpgt " ++ l ++ "\n"
+  show (Ifne (Label l))    = " ifne " ++ l ++ "\n"
+  show (Goto (Label l))    = " goto " ++ l ++ "\n"
+  show InvokePrint         = " invokestatic Output/print(I)V\n"
+  show InvokeRead          = " invokestatic Output/read()I\n"
