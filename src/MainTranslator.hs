2485daf170f70facb29baae8c4506bb432e01ae8
koraynilay <koray.fra@gmail.com>
2026-01-25 02:59:56 +0100

    Move src files to src/ and make better file structure, add MainTranslator.hs

---
+++ src/MainTranslator.hs
@@ -0,0 +1,34 @@
+import Token
+import Parser
+import Lexer
+import CodeGen (toJasmin)
+
+import System.Environment (getArgs, getProgName)
+
+usage :: String -> IO String
+usage s = return ("Usage: " ++ s ++ " FILE")
+
+main :: IO ()
+main = do
+        args <- getArgs
+        case args of
+                [] -> getProgName >>= usage >>= putStrLn
+                as -> handleFiles as
+
+handleFiles :: [String] -> IO ()
+handleFiles [] = return ()
+handleFiles (f:fs) = handleFile f >> handleFiles fs
+
+handleFile :: String -> IO ()
+handleFile fn = do f <- readFile fn
+                   ts <- matchIO fn f
+                   --print ts
+                   let r = parse ts
+                   --putStrLn $ fn ++ ": " ++ show r
+                   putStr $ toJasmin r
+--handleFile fn = readFile fn >>= matchIO fn >>= \x -> putStrLn (fn ++ ": " ++ show (parseDebug x))
+--handleFile fn = do f <- readFile fn
+--                   ts <- matchIO fn f
+--                   print ts
+--                   let r = parse ts
+--                   putStrLn $ fn ++ ": " ++ show r
