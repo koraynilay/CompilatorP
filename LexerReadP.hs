75d84cedf049094c79ce3171ff28cd20a7abd943
koraynilay <koray.fra@gmail.com>
2026-01-28 23:28:33 +0100

    Fix LexerReadP, should be correct now (hopefully)

--- LexerReadP.hs
+++ LexerReadP.hs
@@ -17,16 +17,22 @@ lexDebug = readP_to_S match
 
 -- like (>>) but skipping all spaces and comments
 (>|) :: ReadP a -> ReadP b -> ReadP b
-f >| g = f >> skipSpaces >> many (skipComments >> skipSpaces) >> g
+f >| g = f >> skipWhite >> g
 infixl 1 >|
 
 -- like (>|) but requiring at least 1 space before skipping
 (>!) :: ReadP a -> ReadP b -> ReadP b
-f >! g = f >> optional skipComments >> munch1 isSpace >| g
+f >! g = f >> skipWhite1 >| g
 infixl 1 >!
 
-skipComments :: ReadP String
-skipComments = skipLineComment +++ skipMultComment
+skipWhite :: ReadP ()
+skipWhite = skipSpaces >> (skipComments >> skipWhite) <++ return ()
+
+skipWhite1 :: ReadP ()
+skipWhite1 = optional skipComments >> munch1 isSpace >> return ()
+
+skipComments :: ReadP ()
+skipComments = (skipLineComment +++ skipMultComment) >> return ()
 
 skipLineComment :: ReadP String
 skipLineComment = string "//" >> manyTill get (void (char '\n') +++ eof)
@@ -34,10 +40,19 @@ skipLineComment = string "//" >> manyTill get (void (char '\n') +++ eof)
 skipMultComment :: ReadP String
 skipMultComment = string "/*" >> manyTill get (string "*/")
 
+getSkip :: ReadP String -> ReadP String
+getSkip lexer = do s <- lexer
+                   skipWhite
+                   return s
+
 -- ([a-zA-Z]|_+[a-zA-Z0-9])[a-zA-Z0-9_]*
 getId :: ReadP String
-getId = do c <- (satisfy isAlpha >>= \c -> return [c]) +++ munch1 ('_' ==)
-           x <- many (satisfy isAlpha +++ satisfy isDigit +++ satisfy ('_' ==))
+getId = do c <- (do c <- satisfy isAlpha
+                    return [c])
+                +++ (do u <- munch1 ('_' ==)
+                        c <- satisfy isAlpha
+                        return $ u ++ [c])
+           x <- munch (\c -> isAlpha c || isDigit c || '_' == c)
            return $ c ++ x
 
 -- [0-9]+
@@ -47,39 +62,42 @@ getNum = munch1 isDigit
 -- prods
 
 match :: ReadP [Token]
-match = do ts <- many1 matchTok
+match = do skipWhite
+           ts <- many1 matchTok
            eof
            return $ ts ++ [EOF]
 
 matchTok :: ReadP Token
-matchTok = (string "print"       >| return Print)
-       <|> (string "while"       >| return While)
-       <|> (string "do"          >| return Do)
-       <|> (string "conditional" >| return Conditional)
-       <|> (string "case"        >| return Case)
-       <|> (string "break"       >| return Break)
-       <|> (string "default"     >| return Default)
-       <|> (string "user"        >| return UserInput)
-       <|> (Identifier <$> getId)
-       <|> (Number . read <$> getNum)
-       <|> (string ":=" >| return Assignment)
-       <|> (string "&&" >| return Conjunction)
-       <|> (string "||" >| return Disjunction)
-       <|> (string "==" >| return Equal)
-       <|> (string "!=" >| return NotEqual)
-       <|> (string "<=" >| return LessEqual)
-       <|> (string ">=" >| return GreaterEqual)
-       <|> (string "<" >| return LessThan)
-       <|> (string ">" >| return GreaterThan)
-       <|> (string "(" >| return ParenOpen)
-       <|> (string ")" >| return ParenClose)
-       <|> (string "[" >| return BracketOpen)
-       <|> (string "]" >| return BracketClose)
-       <|> (string "{" >| return CurlyOpen)
-       <|> (string "}" >| return CurlyClose)
-       <|> (string "+" >| return Plus)
-       <|> (string "-" >| return Minus)
-       <|> (string "*" >| return Multiply)
-       <|> (string "/" >| return Divide)
-       <|> (string ";" >| return Semicolon)
-       <|> (string "," >| return Comma)
+matchTok = (do s <- getSkip getId
+               case s of
+                 "print"       -> return Print
+                 "while"       -> return While
+                 "do"          -> return Do
+                 "conditional" -> return Conditional
+                 "case"        -> return Case
+                 "break"       -> return Break
+                 "default"     -> return Default
+                 "user"        -> return UserInput
+                 var           -> return (Identifier var))
+       <++ (Number . read <$> (getSkip getNum))
+       <++ (string ":=" >| return Assignment)
+       <++ (string "&&" >| return Conjunction)
+       <++ (string "||" >| return Disjunction)
+       <++ (string "==" >| return Equal)
+       <++ (string "!=" >| return NotEqual)
+       <++ (string "<=" >| return LessEqual)
+       <++ (string ">=" >| return GreaterEqual)
+       <++ (string "<" >| return LessThan)
+       <++ (string ">" >| return GreaterThan)
+       <++ (string "(" >| return ParenOpen)
+       <++ (string ")" >| return ParenClose)
+       <++ (string "[" >| return BracketOpen)
+       <++ (string "]" >| return BracketClose)
+       <++ (string "{" >| return CurlyOpen)
+       <++ (string "}" >| return CurlyClose)
+       <++ (string "+" >| return Plus)
+       <++ (string "-" >| return Minus)
+       <++ (string "*" >| return Multiply)
+       <++ (string "/" >| return Divide)
+       <++ (string ";" >| return Semicolon)
+       <++ (string "," >| return Comma)
